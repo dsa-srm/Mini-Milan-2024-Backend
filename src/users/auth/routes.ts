c4749cbea0ed1a1023334b170467510b0951c00c
Aditya <aditya298k@gmail.com>
2024-01-06 20:00:17 +0530

    Find bookings

--- src/users/auth/routes.ts
+++ src/users/auth/routes.ts
@@ -1,13 +1,25 @@
 import { Router } from "express";
 import UsersAuthController from "./controller";
 import IUserAuthValidation from "./middleware";
+import rateLimiter from "express-rate-limit";
+
+//Creating a rate limiter
+const limiter = rateLimiter({
+	//Amount of requests per window
+	max: 3,
+	//Window size in ms
+	windowMs: 15 * 60 * 1000, //15 mins
+	//Message on error
+	message: "Too many requests from this IP, try again in 15mins !!",
+});
+
 const router: Router = Router();
 
 const { execute } = new UsersAuthController();
 const { protect } = new IUserAuthValidation();
 
-router.post("/login", execute);
-router.post("/signup", execute);
+router.post("/login", limiter, execute);
+router.post("/signup", limiter, execute);
 router.get("/current", protect, execute);
 router.get("/:id", protect, execute);
 router.delete("/:id", protect, execute);
